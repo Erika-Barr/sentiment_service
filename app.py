22b8e429a2e26415de440c66c7592035966e88d2
Erika Barr <EB@erikas-mbp.attlocal.net>
2017-11-20 09:48:21 -0600

    added route

--- app.py
+++ app.py
@@ -3,15 +3,25 @@ from flask_restful import Resource, Api
 from flask_jsonpify import jsonify
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
-class Test(Resource):
-    def get(self):
-        test = 'this is a test'
-        return jsonify(test)
+#class Test(Resource):
+#    def get(self):
+#        test = 'this is a test'
+#        return jsonify(test)
 
 
-api.add_resource(Test, '/test')
+#api.add_resource(Test, '/test')
+
+@app.route('/')
+def index():
+    return jsonify('test')
+
+@app.route('/analyze/<twitter>')
+def getTwitterHandle(twitter=None):
+    ai_service = 'AI did a sentiment analysis on twitter handle: {}'.format(twitter)
+    return jsonify(ai_service)
+
 
 if __name__ == '__main__':
     #app.run(port='5002')
